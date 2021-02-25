e43e3a4d45e0073892b5f2dd672ed709a33d9910
Stefan Tan <stefantan39@yahoo.com>
2021-02-24 21:44:51 -0500

    added comments

--- 01_bmi/PersonInfo.java
+++ 01_bmi/PersonInfo.java
@@ -1,5 +1,13 @@
-import java.util.GregorianCalendar;
+import java.util.GregorianCalendar; // used to get the current year
 
+/**
+ * The PersonInfo class stores important information about a person including the 
+ * name, year of birth, height, and weight. This information will be used to calculate 
+ * the BMI of the person. This along with the BMI classification and the rest of the 
+ * information will be printed out through the TestBMI class.
+ * 
+ * @author Stefan Tan
+ */
 public class PersonInfo {
 
     private String name;
@@ -7,10 +15,26 @@ public class PersonInfo {
     private double height;
     private double weight;
 
+    /**
+     * This is the default constructor of the PersonInfo class and is called when the
+     * PersonInfo class is instantiated without any parameters provided. When called, it
+     * sets the member variables, name to an empty string, and yearOfBirth, height, and weight
+     * to zero.
+     */
     public PersonInfo() {
         this("", 0, 0, 0);
     }
 
+    /**
+     * This is the non-default constructor of the PersonInfo class and is called when the
+     * PersonInfo class is instantiated with parameters provided. When called, it
+     * sets the member variables, name, yearOfBirth, height, and weight to their respective
+     * values that were provided.
+     * @param name The member variable name will be set to this String value.
+     * @param yearOfBirth The member variable yearOfBirth will be set to this int value.
+     * @param height The member variable height will be set to this double value.
+     * @param weight The member variable weight will be set to this double value.
+     */
     public PersonInfo(String name, int yearOfBirth, double height, double weight) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
@@ -18,46 +42,96 @@ public class PersonInfo {
         this.weight = weight;
     }
 
+    /**
+     * This is the accessor method for the member variable name. It returns the String value
+     * stored in the member variable name.
+     * @return String This returns the current String stored in the member variable name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This is the mutator method for the member variable name. It sets the member variable name to
+     * what is provided in the parameter.
+     * @param name The member variable name will be set to this String value.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This is the accessor method for the member variable yearOfBirth. It returns the int value
+     * stored in the member variable yearOfBirth.
+     * @return int This returns the current int stored in the member variable yearOfBirth.
+     */
     public int getYearOfBirth() {
         return yearOfBirth;
     }
 
+    /**
+     * This is the mutator method for the member variable yearOfBirth. It sets the member variable yearOfBirth 
+     * to what is provided in the parameter.
+     * @param yearOfBirth The member variable yearOfBirth will be set to this int value.
+     */
     public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
     }
 
+    /**
+     * This is the accessor method for the member variable height. It returns the double value
+     * stored in the member variable height.
+     * @return double This returns the current double stored in the member variable height.
+     */
     public double getHeight() {
         return height;
     }
 
+    /**
+     * This is the mutator method for the member variable height. It sets the member variable height 
+     * to what is provided in the parameter.
+     * @param height The member variable height will be set to this double value.
+     */
     public void setHeight(double height) {
         this.height = height;
     }
 
+    /**
+     * This is the accessor method for the member variable weight. It returns the double value
+     * stored in the member variable weight.
+     * @return double This returns the current double stored in the member variable weight.
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * This is the mutator method for the member variable weight. It sets the member variable weight 
+     * to what is provided in the parameter.
+     * @param weight The member variable weight will be set to this double value.
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * This method is used to calculate the age of the person using the member variable yearOfBirth
+     * and the current year.
+     * @return int This returns the age of the person.
+     */
     public int calculateAge() {
-        int currentYear = new GregorianCalendar().get(GregorianCalendar.YEAR);
-        int age = currentYear - yearOfBirth;
+        int currentYear = new GregorianCalendar().get(GregorianCalendar.YEAR); // used to get current year
+        int age = currentYear - yearOfBirth; // age is calculated by subtracting current year and year of birth
         return age;
     }
 
+    /**
+     * This method is used to calculate the BMI of the person using the member variables weight
+     * and height. The formula used to calculate the BMI is the weight divided by the height squared.
+     * @return double This returns the BMI of the person.
+     */
     public double calculateBMI() {
-        double bmi = weight / (height * height);
+        double bmi = weight / (height * height); // BMI is calculated by dividing the weight by the height squared
         return bmi;
     }
 }
--- 01_bmi/TestBMI.java
+++ 01_bmi/TestBMI.java
@@ -1,29 +1,57 @@
-import java.util.Scanner;
+import java.util.Scanner; // used Scanner to read user's inputs
 
+/**
+ * The TestBMI class is uses the information provided by the user to input the
+ * information necessary for the PersonInfo class through the use of the Scanner class. 
+ * Using the information provided by the user, TestBMI will classify the BMI as "Underweight", 
+ * "Normal Weight", "OverWeight", or "Obese" and print this out along with the rest of the 
+ * information given by the user.
+ * 
+ * @author Stefan Tan
+ */
 public class TestBMI {
 
+    /**
+     * This method is used to classify the person's BMI based on the value of the BMI. If the
+     * BMI is less than 18.5, then the person is classified as "Underweight", if the BMI is
+     * between 18.5 and 25, then the person is classified as "Normal Weight", if the BMI is 
+     * between 25 and 30, then the person is classified as "Overweight", and if the BMI is
+     * than 30, then the person is classified as "Obese".
+     * @param bmi This double value is used to determine the classification that is returned.
+     * @return String This returns the classification based on BMI.
+     */
     public static String classifyBMI(double bmi) {
-        if (bmi < 18.5) {
+        if (bmi < 18.5) { // BMI is less than 18.5
             return "Underweight";
         }
-        else if (bmi < 25) {
+        else if (bmi < 25) { // BMI is between 18.5 and 25
             return "Normal Weight";
         }
-        else if (bmi < 30) {
+        else if (bmi < 30) { // BMI is between 25 and 30
             return "Overweight";
         }
-        else {
+        else { // BMI is greater than 30
             return "Obese";
         }
     }
 
+    /**
+     * This is the main method that creates two instances of the PersonInfo class, one using
+     * the default constructor, and the other using the non-default constructor. The user inputs
+     * information for these instances through the use of the Scanner class. The person's information 
+     * along with the calculated age and classification of BMI are then printed out.
+     * @param args This is an array holding the command line arguments. It is not used in this case.
+     */
     public static void main(String[] args) {
 
+        // Instantiating scanner so that it reads the user's inputs
         Scanner scanner = new Scanner(System.in);
 
+        // Creating an instance of PersonInfo using the default constructor
         PersonInfo person1 = new PersonInfo();
 
-        System.out.printf("\nFirst Instance Prompts\n");
+        // Reads user's inputs for the first instance of PersonInfo
+        System.out.printf("%nFirst Instance Prompts%n");
         System.out.printf("Enter person's name: ");
         String name1 = scanner.nextLine();
         System.out.printf("Enter person's year of birth: ");
@@ -33,12 +61,14 @@ public class TestBMI {
         System.out.printf("Enter person's weight in kilograms: ");
         double weight1 = scanner.nextDouble();
 
+        // Using mutator methods to set the member variables of the first instance of PersonInfo
         person1.setName(name1);
         person1.setYearOfBirth(yearOfBirth1);
         person1.setHeight(height1);
         person1.setWeight(weight1);
 
-        System.out.printf("\nSecond Instance Prompts\n");
+        // Reads user's inputs for the second instance of PersonInfo
+        System.out.printf("%nSecond Instance Prompts%n");
         System.out.printf("Enter person's name: ");
         scanner.nextLine(); // Gets rid of the new line input left by scanner.nextDouble()
         String name2 = scanner.nextLine();
@@ -49,22 +79,25 @@ public class TestBMI {
         System.out.printf("Enter person's weight in kilograms: ");
         double weight2 = scanner.nextDouble();
 
+        // Creating an instance of PersonInfo using the non-default constructor
         PersonInfo person2 = new PersonInfo(name2, yearOfBirth2, height2, weight2);
 
-        System.out.printf("\n1st instance Output\n");
+        // Prints out information for the first instance of PersonInfo
+        System.out.printf("%n1st instance Output%n");
         System.out.printf("%16s%s%n", "Full Name: ", person1.getName());
         System.out.printf("%16s%d%n", "Age: ", person1.calculateAge());
         System.out.printf("%16s%.2f%n", "Height: ", person1.getHeight());
         System.out.printf("%16s%.2f%n", "Weight: ", person1.getWeight());
         System.out.printf("%16s%s%n", "Classification: ", classifyBMI(person1.calculateBMI()));
 
-        System.out.printf("\n2nd instance Output\n");
+        // Prints out information for the second instance of PersonInfo
+        System.out.printf("%n2nd instance Output%n");
         System.out.printf("%16s%s%n", "Full Name: ", person2.getName());
         System.out.printf("%16s%d%n", "Age: ", person2.calculateAge());
         System.out.printf("%16s%.2f%n", "Height: ", person2.getHeight());
         System.out.printf("%16s%.2f%n", "Weight: ", person2.getWeight());
         System.out.printf("%16s%s%n%n", "Classification: ", classifyBMI(person2.calculateBMI()));
 
-        scanner.close();
+        scanner.close(); // Closes scanner
     }
 }
